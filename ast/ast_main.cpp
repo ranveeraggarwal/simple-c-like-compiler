67e66f5f8ffe8cf06d225a2225725f85221a6baa
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-02-04 16:10:43 +0530

    ast init1

--- ast/ast_main.cpp
+++ ast/ast_main.cpp
@@ -1,14 +1,19 @@
 class abstract_astnode
 {
-	public:
-		virtual void print () = 0;
-		virtual std::string generate_code(const symbolTable&) = 0;
-		virtual basic_types getType() = 0;
-		virtual bool checkTypeofAST() = 0;
-	protected:
-		virtual void setType(basic_types) = 0;
-	private:
-		typeExp astnode_type;
+public:
+	virtual void print () = 0;
+	virtual std::string generate_code(const symbolTable&) = 0;
+	virtual basic_types getType() = 0;
+	virtual bool checkTypeofAST() = 0;
+protected:
+	virtual void setType(basic_types) = 0;
+private:
+	typeExp astnode_type;
+};
+
+class stmtast: public abstract_astnode
+{
+public:
 };
 
 int main(int argc, char const *argv[])
