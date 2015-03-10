a9595c62613d5386c8baa7278a758a5e057cd8e7
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-03-10 14:36:42 +0530

    cpp daala

---
+++ semantic-analyser/symbol_table.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class symbolTable
+{
+	
+};
+
+class gst: public symbolTable
+{
+	vector<lst> lstList;
+	lst* getLst(string funcName)
+	{
+
+	}
+};
+
+class lst: public symbolTable
+{
+	
+};
