47c2d0ece22c31afcf64f7b31111341feccbdc97
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-04-15 16:44:50 +0530

    pushe push pus pu p $

--- machine-code/ast.h
+++ machine-code/ast.h
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 extern int labelNumber;
+extern map<string,string> fallThrough;
 
 string nextLabel();
+
+class Instruction
+{
+public:
+	Instruction();
+	~Instruction();
+};
